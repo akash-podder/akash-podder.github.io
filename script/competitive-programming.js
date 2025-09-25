0d93a279e5e5c3c8c743c7dcad8e70563b13303e
AkashPodder <anantaakash.podder@gmail.com>
2025-09-25 12:38:08 +0600

    - added `total-solved` in `competitive-programming.json` to Parse the TOTAL Problem Solved Field Dynamically

--- script/competitive-programming.js
+++ script/competitive-programming.js
@@ -2,6 +2,7 @@ fetch('https://gist.githubusercontent.com/akash-podder/ed35d2bd076088f87c5693833
     .then(res => res.json())
     .then(
         data => {
+            document.getElementById('total-solved').innerText = `Overall solved ${data.total} different problems`;
             document.getElementById('codeforces-solved').innerText = `Codeforces: ${data.codeforces} solves`;
             document.getElementById('leetcode-solved').innerText = `Leetcode: ${data.leetcode} solves`;
             document.getElementById('vjudge-solved').innerText = `Vjudge: ${data.vjudge} solves`;
